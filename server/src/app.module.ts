70366b3c26f552c9a7e15d5dbf767383ce03941f
The73756 <the73756the@gmail.com>
2022-12-11 14:58:52 +0900

    add file upload & file serve func

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TrackModule } from './track/track.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { FileModule } from './file/file.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/music-platform'), TrackModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, 'static'),
+    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/music-platform'),
+    TrackModule,
+    FileModule,
+  ],
 })
 export class AppModule {}
