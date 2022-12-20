909c8a947e45e574427c7163c91b5bb3528201a4
The73756 <the73756the@gmail.com>
2022-12-20 15:43:08 +0900

    add one track page

--- client/pages/tracks/[id].tsx
+++ client/pages/tracks/[id].tsx
@@ -1,7 +1,53 @@
 import MainLayout from '../../layouts/MainLayout';
+import { ITrack } from '../../types/track';
+import { Box, Button, Grid, TextField } from '@mui/material';
+import { useRouter } from 'next/router';
+import { Routes } from '../../types/routes';
 
 const TrackPage = () => {
-  return <MainLayout>Track page lol</MainLayout>;
+  const track: ITrack = {
+    _id: '1',
+    artist: 'Artist 1',
+    name: 'Track 1',
+    text: 'Text 1',
+    listens: 1,
+    picture: 'http://localhost:5000/image/24c01937-90cd-4169-96bd-88d1f9566868.webp',
+    audio: 'http://localhost:5000/audio/7b56e367-58d8-4d4a-ba37-2fc0fe11b5c9.ogg',
+    comments: [],
+  };
+  const router = useRouter();
+
+  return (
+    <MainLayout>
+      <Button variant="outlined" onClick={() => router.push(Routes.HOME)}>
+        Назад
+      </Button>
+      <Grid container p="10px">
+        <img src={track.picture} alt={`Обложка к треку ${track.name}`} width={200} height={200} />
+        <Box>
+          <h1>Название - {track.name}</h1>
+          <h2>Исполнитель - {track.artist}</h2>
+          <p>Прослушиваний - {track.listens}</p>
+        </Box>
+      </Grid>
+      <h2>Слова в треке</h2>
+      <p>{track.text}</p>
+      <h2>Комментарии</h2>
+      <Grid container>
+        <TextField label="Ваше имя" fullWidth margin="normal" />
+        <TextField label="Комментарий" multiline fullWidth rows={4} />
+        <Button>Отправить</Button>
+      </Grid>
+      <div>
+        {track.comments.map((comment) => (
+          <article>
+            <p>автор - {comment.username}</p>
+            <p>text - {comment.text}</p>
+          </article>
+        ))}
+      </div>
+    </MainLayout>
+  );
 };
 
 export default TrackPage;
